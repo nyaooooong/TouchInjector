30e77ada84c669c3d7bbe2a27a5a3bea1ee86f06
nyaoooooong <nyaoooooong@gmail.com>
2021-06-07 11:29:44 +0900

    Invoke service by volume key

--- app/src/main/java/com/nrw/touchinjector/MainActivity.kt
+++ app/src/main/java/com/nrw/touchinjector/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.nrw.touchinjector
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
 import com.nrw.touchinjector.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +17,17 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d("Key Code:", keyCode.toString())
+        when (keyCode) {
+            KeyEvent.KEYCODE_VOLUME_DOWN -> {
+                Intent(this, TouchInjectorService::class.java).also { intent -> startService(intent)}
+            }
+            KeyEvent.KEYCODE_VOLUME_UP -> {
+                Intent(this, TouchInjectorService::class.java).also { intent -> stopService(intent)}
+            }
+        }
+        return false // If you handled the event, return true. If you want to allow the event to be handled by the next receiver, return false.
     }
 }
