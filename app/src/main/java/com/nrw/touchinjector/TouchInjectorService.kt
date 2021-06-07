c5f302df3b23a7156a738207d9f2f9f9987e9ce7
nyaoooooong <nyaoooooong@gmail.com>
2021-06-07 10:22:01 +0900

    Add TouchInjector Service

---
+++ app/src/main/java/com/nrw/touchinjector/TouchInjectorService.kt
@@ -0,0 +1,12 @@
+package com.nrw.touchinjector
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class TouchInjectorService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
